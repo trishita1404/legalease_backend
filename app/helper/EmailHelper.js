aa23ae62450fc6f4c3aec8f9dbf63c662ef8624f
trishita1404 <trishitacontai@gmail.com>
2026-05-06 22:51:53 +0530

    Fixed nodemailer timeout

--- app/helper/EmailHelper.js
+++ app/helper/EmailHelper.js
@@ -1,25 +1,27 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 const SendEmail = async (EmailTo, EmailText, EmailSubject) => {
-    let transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
-        },
-        tls: { rejectUnauthorized: false },
-    });
+  let transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
+  });
 
-    let mailOptions = {
-        from: `LegalEase+ <${process.env.EMAIL_FROM}>`,
-        to: EmailTo,
-        subject: EmailSubject,
-        text: EmailText,
-    };
+  let mailOptions = {
+    from: `LegalEase+ <${process.env.EMAIL_FROM}>`,
+    to: EmailTo,
+    subject: EmailSubject,
+    text: EmailText,
+  };
 
-    return await transporter.sendMail(mailOptions);
+  return await transporter.sendMail(mailOptions);
 };
 
 module.exports = SendEmail;
