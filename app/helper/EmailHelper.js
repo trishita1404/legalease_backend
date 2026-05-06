0da50f03936310881948a995b8a0756fcae2254a
trishita1404 <trishitacontai@gmail.com>
2026-05-06 23:10:44 +0530

    Fixed Gmail IPv6 issue

--- app/helper/EmailHelper.js
+++ app/helper/EmailHelper.js
@@ -1,27 +1,26 @@
 const nodemailer = require("nodemailer");
 
 const SendEmail = async (EmailTo, EmailText, EmailSubject) => {
-  let transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 10000,
-  });
 
-  let mailOptions = {
-    from: `LegalEase+ <${process.env.EMAIL_FROM}>`,
-    to: EmailTo,
-    subject: EmailSubject,
-    text: EmailText,
-  };
+    let transporter = nodemailer.createTransport({
+        service: "gmail",
 
-  return await transporter.sendMail(mailOptions);
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+        },
+
+        family: 4,
+    });
+
+    let mailOptions = {
+        from: `LegalEase+ <${process.env.EMAIL_FROM}>`,
+        to: EmailTo,
+        subject: EmailSubject,
+        text: EmailText,
+    };
+
+    return await transporter.sendMail(mailOptions);
 };
 
 module.exports = SendEmail;
