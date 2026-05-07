5d456dd83f665c5a268a4fba9b3c99703953b2a9
trishita1404 <trishitacontai@gmail.com>
2026-05-07 20:11:34 +0530

    otp issue

--- app/helper/EmailHelper.js
+++ app/helper/EmailHelper.js
@@ -4,20 +4,20 @@ const SendEmail = async (EmailTo, EmailText, EmailSubject) => {
 
     try {
 
-        let transporter = nodemailer.createTransport({
+        const transporter = nodemailer.createTransport({
 
-            service: "gmail",
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,
 
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS,
             },
 
-            family: 4, // FORCE IPV4 (IMPORTANT FIX)
+            family: 4,
 
         });
-
-        await transporter.verify();
 
         let mailOptions = {
             from: `LegalEase+ <${process.env.EMAIL_FROM}>`,
@@ -34,7 +34,7 @@ const SendEmail = async (EmailTo, EmailText, EmailSubject) => {
 
     } catch (error) {
 
-        console.log("EMAIL HELPER ERROR:");
+        console.log("EMAIL ERROR:");
         console.log(error);
 
         throw error;
