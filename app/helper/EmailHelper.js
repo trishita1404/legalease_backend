f94efcdb17c91388cde28da256a04474c1a9ba41
trishita1404 <trishitacontai@gmail.com>
2026-05-07 20:36:05 +0530

    helper change

--- app/helper/EmailHelper.js
+++ app/helper/EmailHelper.js
@@ -1,44 +1,25 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 
 const SendEmail = async (EmailTo, EmailText, EmailSubject) => {
+    let transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+        },
+        tls: { rejectUnauthorized: false },
+    });
 
-    try {
+    let mailOptions = {
+        from: `LegalEase+ <${process.env.EMAIL_FROM}>`,
+        to: EmailTo,
+        subject: EmailSubject,
+        text: EmailText,
+    };
 
-        const transporter = nodemailer.createTransport({
-
-            host: "smtp.gmail.com",
-            port: 465,
-            secure: true,
-
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS,
-            },
-
-            family: 4,
-
-        });
-
-        let mailOptions = {
-            from: `LegalEase+ <${process.env.EMAIL_FROM}>`,
-            to: EmailTo,
-            subject: EmailSubject,
-            text: EmailText,
-        };
-
-        const info = await transporter.sendMail(mailOptions);
-
-        console.log("EMAIL SENT:", info.response);
-
-        return info;
-
-    } catch (error) {
-
-        console.log("EMAIL ERROR:");
-        console.log(error);
-
-        throw error;
-    }
+    return await transporter.sendMail(mailOptions);
 };
 
 module.exports = SendEmail;
